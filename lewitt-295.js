d0238a5913e2f2224ba790a636b98c8b424078ac
Nathalia Kasman <34670412+natkasman@users.noreply.github.com>
2018-02-09 22:59:29 -0800

    Create lewitt-295.js

---
+++ lewitt-295.js
@@ -0,0 +1,16 @@
+function vertical() {
+  strokeWeight(1);
+  stroke(255);
+  noFill();
+  
+  rect(20, 20, 300, 300)
+  ellipse(170, 170, 300, 300)
+  triangle(20, 320, 170, 22, 320, 320)
+  triangle(20, 320, 80, 22, 80, 320)
+  triangle(320, 320, 240, 22, 240, 320)
+
+
+}
+  
+background(5);
+vertical(0, 0);
