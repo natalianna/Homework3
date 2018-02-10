d44ce919aa9494c900259b5386ac659dc19094e5
Nathalia Kasman <34670412+natkasman@users.noreply.github.com>
2018-02-09 23:52:51 -0800

    Create lewitt-154.js

---
+++ lewitt-154.js
@@ -0,0 +1,13 @@
+	function rectang() {
+  rect(20, 20, 300, 300);
+  }
+	
+	function horizontal() {
+  colorMode(RGB);
+  stroke (255, 0, 0);
+  line(20, 150, 150, 150);
+  }
+
+background(255);
+rectang();
+horizontal();
